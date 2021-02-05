51d059fa2134d8a5eafc14293a21f95913a739a7
arul hasbi <arulhasbi@aruls-MacBook-Pro.local>
2021-02-06 04:38:05 +0800

    setting up metadata

---
+++ functions.php
@@ -0,0 +1,28 @@
+<?php
+
+// add dynamic title tag support
+function custom_theme_support() {
+    add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'custom_theme_support');
+
+function custom_register_styles() {
+    $version = wp_get_theme()->get('Version');
+    wp_enqueue_style('ahasbi-bootstrap', "https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css", array(), '4.6.0', 'all');
+    wp_enqueue_style('ahasbi-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css", array(), '5.15.2', 'all');
+    wp_enqueue_style('ahasbi-custom', get_template_directory_uri() . "style.css", array(), $version, 'all');
+}
+
+add_action('wp_enqueue_scripts', 'custom_register_styles');
+
+function custom_registe_scripts() {
+    wp_enqueue_script('ahasbi-jquery', 'https://code.jquery.com/jquery-3.5.1.slim.min.js', array(), '3.5.1', 'all', true);
+    wp_enqueue_script('ahasbi-popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js', array(), '1.16.1', 'all', true);
+    wp_enqueue_script('ahasbi-bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js', array(), '4.6.0', 'all', true);
+    wp_enqueue_script('ahasbi-javascript',  get_template_directory_uri() . "assets/js/main.js", array(), '1.0', 'all', true);
+}
+
+add_action('wp_enqueue_scripts', 'custom_registe_scripts');
+
+?>
---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-quiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Personal website">
+        <meta name="author" content="https://linkedin.com">
+        <?php
+            wp_head();
+        ?>
+    </head>
+    <body>
+        <header>
+             menu here
+        </header>
